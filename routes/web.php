08e859e8ccd02964efa96dd4f9482606e307affa
Steven Friend <stevenmarkfriend@gmail.com>
2024-02-05 14:28:57 +0900

    レビュー機能の追加

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,8 @@ use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('reviews', [ReviewController::class, 'store'])->name('reviews.store');
 
 Route::resource('products', ProductController::class)->middleware(['auth', 'verified']);
 Auth::routes(['verify' => true]);
